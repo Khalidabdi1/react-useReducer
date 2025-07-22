84b0c3c81f14618cd76d2ea7661062b9c5d6d155
Khalid Abdi <xamza2009@gmail.com>
2025-07-22 17:29:01 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Calculator from './component/Calculator';
-import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
-    <Analytics>
+   
     <div className="App">
     <Calculator/>
     </div>
-    </Analytics>
+  
   );
 }
 
