e0ac44f9db68a2465d074a1105d02baaf1da70fa
Khalid abdi <khalidabdi@Khalids-MacBook-Air.local>
2025-07-22 16:57:21 +0300

    make useReduce in file

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,24 +1,6 @@
 import { useReducer, useState } from "react"
 
-function Show(current,setCurrent){
-console.log(current)
-
-let {firstNumber,secondNumber}=setCurrent.payload
-if(setCurrent.type==="added"){
-    return Number.parseInt(firstNumber)+Number.parseInt(secondNumber)
-}else if(setCurrent.type==="Substract"){
-        return Number.parseInt(firstNumber) - Number.parseInt(secondNumber)
-
-}else if(setCurrent.type==="multiply"){
-        return Number.parseInt(firstNumber) * Number.parseInt(secondNumber)
-
-}else if(setCurrent.type==="divide"){
-        return Number.parseInt(firstNumber) /  Number.parseInt(secondNumber)
-
-}
-
-
-}
+import { Show } from "./CalculatorReduce."
 
 export default function Calculator(){
     const[firstNumber,setFirstNumbser]=useState(null)
---
+++ src/component/CalculatorReduce..js
@@ -0,0 +1,19 @@
+export function Show(current,setCurrent){
+console.log(current)
+
+let {firstNumber,secondNumber}=setCurrent.payload
+if(setCurrent.type==="added"){
+    return Number.parseInt(firstNumber)+Number.parseInt(secondNumber)
+}else if(setCurrent.type==="Substract"){
+        return Number.parseInt(firstNumber) - Number.parseInt(secondNumber)
+
+}else if(setCurrent.type==="multiply"){
+        return Number.parseInt(firstNumber) * Number.parseInt(secondNumber)
+
+}else if(setCurrent.type==="divide"){
+        return Number.parseInt(firstNumber) /  Number.parseInt(secondNumber)
+
+}
+
+//Calculator Reduce
+}
